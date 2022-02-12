f349d88b023d160d642113017202d1540fd02e62
IAmSinix <99491942+IAmSinix@users.noreply.github.com>
2022-02-12 20:37:40 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+        return file.read()
+
+setup(name='SinixDB',
+      version='1.1',
+      description='Simple json type database for python3!',
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      author='IAmSinix',
+      author_email='sinixfeedback@gmail.com',
+      url='https://github.com/IAmSinix/SinixDB',
+      packages = find_packages(exclude = ['examples']),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+          'License :: Apache License 2.0',
+      ],
+      
+      )
